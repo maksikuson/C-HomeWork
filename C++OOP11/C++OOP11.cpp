1a71bef369e0e840abdb7a628b8d3e50dd8aa533
Your Name <you@example.com>
2024-05-27 02:07:28 +0300

    Completed task 11, 12 OOP

--- C++OOP11/C++OOP11.cpp
+++ C++OOP11/C++OOP11.cpp
@@ -1,7 +1,272 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
+
+class Pet 
+{
+protected:
+    string name;
+public:
+    Pet(string petName) : name(petName) {}
+
+    void display() const 
+    {
+        cout << "Pet Name: " << name << endl;
+    }
+};
+
+class Dog : public Pet
+{
+    string owner;
+public:
+    Dog(string dogName, string ownerName) : Pet(dogName), owner(ownerName) {}
+
+    void display() const 
+    {
+        cout << "Dog Name: " << name << endl;
+        cout << "Dog's Owner: " << owner << endl;
+    }
+};
+
+class Cat : public Pet 
+{
+    string ownerC;
+public:
+    Cat(string catName, string ownerCatName) : Pet(catName), ownerC(ownerCatName) {}
+
+    void display() const 
+    {
+        cout << "Cat Name: " << name << endl;
+        cout << "Cat's Owner: " << ownerC << endl;
+    }
+};
+
+class Parrot : public Pet
+{
+    string ownerP;
+public:
+    Parrot(string parrotName, string ownerPname) : Pet(parrotName), ownerP(ownerPname) {}
+
+    void display() const 
+    {
+        cout << "Parrot Name: " << name << endl;
+        cout << "Parrot's Owner: " << ownerP << endl;
+    }
+};
+
+class String
+{
+protected:
+    char* str;
+    size_t length;
+public:
+    String() : str(new char[1] {'\0'}), length(0) {}
+
+    String(const char* str) 
+    {
+        length = strlen(str);
+        this->str = new char[length + 1];
+        strcpy_s(this->str, length + 1, str);
+    }
+
+    String(const String& other) : length(other.length)
+    {
+        str = new char[length + 1];
+        strcpy_s(str, length + 1, other.str);
+    }
+
+    String& operator=(const String& other) 
+    {
+        if (this != &other)
+        {
+            delete[] str;
+            length = other.length;
+            str = new char[length + 1];
+            strcpy_s(str, length + 1, other.str);
+        }
+        return *this;
+    }
+
+    size_t getLength() const 
+    {
+        return length;
+    }
+
+    void clear() 
+    {
+        delete[] str;
+        str = new char[1] {'\0'};
+        length = 0;
+    }
+
+    ~String() 
+    {
+        delete[] str;
+    }
+
+    String operator+(const String& other) const
+    {
+        size_t newLength = length + other.length;
+        char* newStr = new char[newLength + 1];
+        strcpy_s(newStr, newLength + 1, str);
+        strcat_s(newStr, newLength + 1, other.str);
+
+        String result(newStr);
+        delete[] newStr;
+
+        return result;
+    }
+
+    bool operator==(const String& other) const 
+    {
+        return strcmp(str, other.str) == 0;
+    }
+
+    bool operator!=(const String& other) const 
+    {
+        return !(*this == other);
+    }
+
+    void show() const 
+    {
+        cout << "Length: " << length << endl;
+        cout << "String: " << str << endl;
+    }
+};
+
+class BinaryString : public String
+{
+public:
+    BinaryString() : String("00000") {}
+
+    BinaryString(const char* str) : String() 
+    {
+        bool isValid = true;
+        for (size_t i = 0; i < strlen(str); ++i) 
+        {
+            if (str[i] != '0' && str[i] != '1')
+            {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid)
+        {
+            delete[] this->str;
+            length = strlen(str);
+            this->str = new char[length + 1];
+            strcpy_s(this->str, length + 1, str);
+        }
+        else
+        {
+            this->str = new char[1] {'\0'};
+            length = 0;
+        }
+    }
+
+    void negate() 
+    {
+        for (size_t i = 0; i < length; ++i)
+        {
+            str[i] = (str[i] == '0') ? '1' : '0';
+        }
+
+        for (int i = length - 1; i >= 0; --i) 
+        {
+            if (str[i] == '0')
+            {
+                str[i] = '1';
+                break;
+            }
+            else 
+            {
+                str[i] = '0';
+            }
+        }
+    }
+
+    int toInteger() const 
+    {
+        int result = 0;
+        for (size_t i = 0; i < length; ++i)
+        {
+            result = (result << 1) + (str[i] - '0');
+        }
+        return result;
+    }
+
+    void show() const
+    {
+        cout << "Binary String: " << str << endl;
+    }
+};
+
+void displayPets() 
+{
+    Pet p1("My Pet");
+    p1.display();
+
+    Dog d1("Patron", "Maksik");
+    d1.display();
+
+    Cat c1("Tigra", "Maxim");
+    c1.display();
+
+    Parrot per1("Blu", "Maksimiliano");
+    per1.display();
+}
+
+void stringOperations() 
+{
+    String st1("Hello my world");
+    cout << "Length: " << st1.getLength() << endl;
+    st1.show();
+    st1.clear();
+    st1.show();
+
+    String S1("Hello,");
+    String S2(" Teacher");
+    String S3 = S1 + S2;
+    S3.show();
+
+    String s1("Hello");
+    String s2("Hello");
+    String s3("World");
+
+    if (s1 == s2)
+    {
+        cout << "s1 and s2 are equal" << endl;
+    }
+    else 
+    {
+        cout << "s1 and s2 are not equal" << endl;
+    }
+
+    if (s1 == s3)
+    {
+        cout << "s1 and s3 are equal" << endl;
+    }
+    else 
+    {
+        cout << "s1 and s3 are not equal" << endl;
+    }
+}
+
+void binaryStringOperations()
+{
+    BinaryString bs1("101010");
+    bs1.show();
+    cout << "Integer value: " << bs1.toInteger() << endl;
+
+    BinaryString bs2("1010201");
+    bs2.show();
+}
 
 int main()
 {
-
+    displayPets();
+    cout << "*****************************" << endl;
+    stringOperations();
+    cout << "*****************************" << endl;
+    binaryStringOperations();
 }
--- C++OOP12/C++OOP12.cpp
+++ C++OOP12/C++OOP12.cpp
@@ -1,7 +1,190 @@
 #include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
 using namespace std;
+
+//************************TASK1************************
+
+class Equation
+{
+public:
+	virtual void solve() = 0;
+};
+
+class LinearEquation : public Equation 
+{
+private:
+    double a, b;
+
+public:
+    LinearEquation(double a_val, double b_val) : a(a_val), b(b_val) {}
+
+    void solve() override 
+    {
+        if (a == 0) 
+        {
+            if (b == 0)
+                cout << "The equation has infinitely many solutions" << endl;
+            else
+                cout << "The equation has no solution" << endl;
+        }
+        else 
+        {
+            double x = -b / a;
+            cout << "Solution: x = " << x << endl;
+        }
+    }
+};
+
+class QuadraticEquation : public Equation
+{
+private:
+    double a, b, c;
+
+public:
+    QuadraticEquation(double a_val, double b_val, double c_val) : a(a_val), b(b_val), c(c_val) {}
+
+    void solve() override
+    {
+        if (a == 0) 
+        {
+            LinearEquation linear(b, c);
+            linear.solve();
+        }
+        else 
+        {
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0) 
+            {
+                double x1 = (-b + sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - sqrt(discriminant)) / (2 * a);
+                cout << "Solutions: x1 = " << x1 << ", x2 = " << x2 << endl;
+            }
+            else if (discriminant == 0) 
+            {
+                double x = -b / (2 * a);
+                cout << "The equation has one real solution x = " << x << endl;
+            }
+            else 
+            {
+                cout << "The equation has no real solutions" << endl;
+            }
+        }
+    }
+};
+
+//************************TASK2************************
+
+class Shape
+{
+public:
+    virtual void Show() const = 0;
+    virtual void Save(ofstream& file) const = 0;
+    virtual void Load(ifstream& file) = 0;
+    virtual ~Shape() {}
+};
+
+class Square : public Shape
+{
+private:
+    int topLeftX, topLeftY;
+    int sideLength;
+public:
+    Square(int x, int y, int length) : topLeftX(x), topLeftY(y), sideLength(length) {}
+
+    void Show() const override 
+    {
+        cout << "Square: Top Left Corner (" << topLeftX << ", " << topLeftY << "), Side Length: " << sideLength << endl;
+    }
+
+    void Save(ofstream& file) const override 
+    {
+        file << "Square " << topLeftX << " " << topLeftY << " " << sideLength << endl;
+    }
+
+    void Load(ifstream& file) override
+    {
+        file >> topLeftX >> topLeftY >> sideLength;
+    }
+};
+
+void SaveShapes(const vector<Shape*>& shapes, const string& filename) 
+{
+    ofstream outFile(filename);
+    if (outFile.is_open())
+    {
+        for (auto shape : shapes)
+        {
+            shape->Save(outFile);
+        }
+        outFile.close();
+    }
+    else 
+    {
+        cerr << "Unable to open file " << filename << " for writing." << endl;
+    }
+}
+
+
+void LoadShapes(vector<Shape*>& shapes, const string& filename)
+{
+    ifstream inFile(filename);
+    if (inFile.is_open()) 
+    {
+        string type;
+        while (inFile >> type) 
+        {
+            if (type == "Square")
+            {
+                int x, y, length;
+                inFile >> x >> y >> length;
+                shapes.push_back(new Square(x, y, length)); 
+            }
+        }
+        inFile.close();
+    }
+    else 
+    {
+        cerr << "Unable to open file " << filename << " for reading." << endl;
+    }
+}
 
 int main()
 {
+    //Task 1
+
+    /*QuadraticEquation equation1(1, -3, 2);
+    equation1.solve();
+
+    QuadraticEquation equation2(1, -2, 5);
+    equation2.solve();
+
+    LinearEquation equation3(0, 0);
+    equation3.solve();
+
+    LinearEquation equation4(0, 5);
+    equation4.solve();*/
+
+    // Task 2
+
+    vector<Shape*> shapes;
+    shapes.push_back(new Square(5, 7, 5));
+
+    SaveShapes(shapes, "shapes.txt");
+
+    LoadShapes(shapes, "shapes.txt");
+
+    for (auto shape : shapes)
+    {
+        shape->Show();
+    }
+
+    for (auto shape : shapes) 
+    {
+        delete shape;
+    }
+    shapes.clear();
 
 }
